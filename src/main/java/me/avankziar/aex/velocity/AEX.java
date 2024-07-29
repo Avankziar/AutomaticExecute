38d1a07396f3e767f4e93e899ff162e81d73d323
Avankziar <avankziar@gmail.com>
2024-07-29 20:00:41 +0200

    velocity logger .toString change to getId

--- src/main/java/me/avankziar/aex/velocity/AEX.java
+++ src/main/java/me/avankziar/aex/velocity/AEX.java
@@ -58,7 +58,7 @@ public class AEX
     	logger = Logger.getLogger("AEX");
     	PluginDescription pd = server.getPluginManager().getPlugin(pluginname.toLowerCase()).get().getDescription();
         List<String> dependencies = new ArrayList<>();
-        pd.getDependencies().stream().allMatch(x -> dependencies.add(x.toString()));
+        pd.getDependencies().stream().allMatch(x -> dependencies.add(x.getId()));
         //https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=SCC
 		logger.info("  █████╗ ███████╗██╗  ██╗ | Id: "+pd.getId());
 		logger.info(" ██╔══██╗██╔════╝╚██╗██╔╝ | Version: "+pd.getVersion().get());
